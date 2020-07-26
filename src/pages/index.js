6fa1d5aa85e86d781f1fc1c46dfe7cfc477be931
glee3491 <ginalee@gatech.edu>
2020-07-27 00:33:21 +0900

    add final video

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,13 +32,14 @@ const AboutPage = () => {
 
         <h1 className="title" data-splitting="">
           Social Media and
-          <br /> Hate Coments
+          <br /> Hate Comments
         </h1>
 
         <h2 className="subtitle">Social Media Hate Comments, Ethic and Responsibility</h2>
 
         <div className="intro">
-        <iframe title="introvid" width="789" height="444" src="https://www.youtube.com/embed/hyIb_Mur5Gs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
+        <iframe className = "introvid" title="introvid" width="631" height="355" src="https://www.youtube.com/embed/A3E9zed9xWY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="allowfullscreen"></iframe>
+        <br />
           <p>
             <TransitionLink
               exit={{
